859185f6ad7f1028692d6916c90282fc8397c01d
MadSC <luk8397@gmail.com>
2021-10-11 11:49:04 +0300

    added error cathing

--- Tankio Server/TankioServer/Listeners/TCPListener.cs
+++ Tankio Server/TankioServer/Listeners/TCPListener.cs
@@ -28,30 +28,36 @@ namespace TankioServer.Listeners
 
         void ConnectionCallback(IAsyncResult result)
         {
-            HttpListenerContext context = server.EndGetContext(result);
-            //context: provides access to httplistener's response
+            try
+            {
+                HttpListenerContext context = server.EndGetContext(result);
+                //context: provides access to httplistener's response
 
-            HttpListenerResponse response = context.Response;
-            //the response tells the server where to send the datas
+                HttpListenerResponse response = context.Response;
+                //the response tells the server where to send the datas
 
-            string page = Directory.GetCurrentDirectory() + "/HTML" + context.Request.Url.LocalPath;
-            //this will get the page requested by the browser 
-
-
-            if (context.Request.Url.LocalPath == "/")  //if there's no page, we'll say it's index.html
-                page += "index.html";
+                string page = Directory.GetCurrentDirectory() + "/HTML" + context.Request.Url.LocalPath;
+                //this will get the page requested by the browser 
 
 
-            Console.WriteLine(page);
+                if (context.Request.Url.LocalPath == "/")  //if there's no page, we'll say it's index.html
+                    page += "index.html";
 
-            byte[] buffer = File.ReadAllBytes(page);
-            //then we transform it into a byte array
 
-            response.ContentLength64 = buffer.Length;  // set up the messasge's length
-            Stream st = response.OutputStream;  // here we create a stream to send the message
-            st.Write(buffer, 0, buffer.Length); // and this will send all the content to the browser
+                Console.WriteLine(page);
 
-            context.Response.Close();  // here we close the connection
+                byte[] buffer = File.ReadAllBytes(page);
+                //then we transform it into a byte array
+
+                response.ContentLength64 = buffer.Length;  // set up the messasge's length
+                Stream st = response.OutputStream;  // here we create a stream to send the message
+                st.Write(buffer, 0, buffer.Length); // and this will send all the content to the browser
+
+                context.Response.Close();  // here we close the connection
+            }catch(Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
         }
 
         void Listener()
